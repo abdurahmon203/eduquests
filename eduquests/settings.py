588764337d1c6f967b7f1d95f3e90e9b7353c96b
Abdurahmon <nozimovislom101@gmail.com>
2026-05-26 14:31:04 +0500

    Media Settings

--- eduquests/settings.py
+++ eduquests/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_ROOT = [
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
