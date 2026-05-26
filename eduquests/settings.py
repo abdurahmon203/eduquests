da03a84dc7d67af01d3df2b95f47b09131a5d675
Abdurahmon <nozimovislom101@gmail.com>
2026-05-26 14:43:58 +0500

    Levels and Subjects Settings in INSTALLED_APPS

--- eduquests/settings.py
+++ eduquests/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'gamification',
+    'levels',
+    'subjects',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
