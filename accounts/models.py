fffc850230e8b255c972b5a75752ce0a79de7a4d
Abdurahmon <nozimovislom101@gmail.com>
2026-05-28 17:34:52 +0500

    Changed User Modle for Email Verification

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,9 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
     score = models.PositiveIntegerField(default=0)
+    is_verified = models.BooleanField(default=False)
+    otp_code = models.CharField(max_length=6, blank=True, null=True)
+    otp_created_at = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     USERNAME_FIELD = "email"
