7bb04c86d80396185958665fc0163bbd323e91a3
Abdurahmon <nozimovislom101@gmail.com>
2026-05-29 12:37:25 +0500

    Added learn_vidios in views.py

--- levels/views.py
+++ levels/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from subjects.models import Subject
 from gamification.models import UserProgress
+from .models import Level
 
 
 def level_list(request, subject_id):
     subject = get_object_or_404(Subject, id=subject_id)
     levels = subject.levels.all()
-    
+
     completed_level_ids = []
     if request.user.is_authenticated:
         completed_level_ids = list(
             UserProgress.objects.filter(
-                user=request.user,
-                level__subject=subject,
-                is_completed=True
+                user=request.user, level__subject=subject, is_completed=True
             ).values_list("level_id", flat=True)
         )
-        
+
     context = {
         "subject": subject,
         "levels": levels,
@@ -24,3 +23,9 @@ def level_list(request, subject_id):
     }
 
     return render(request, "levels/level_list.html", context)
+
+
+def learn_vidios(request):
+    levels = Level.objects.all()
+
+    return render(request, "levels/learn.html", {"levels": levels})
