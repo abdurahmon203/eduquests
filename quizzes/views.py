7c33c2c8ee6358065768a98464114f1fef9e170b
Abdurahmon <nozimovislom101@gmail.com>
2026-05-29 06:20:05 +0500

    Added quiz_view in Quizzes for cheking

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from levels.models import Level
+from .models import Question
 
-# Create your views here.
+
+def quiz_view(request, level_id):
+    level = get_object_or_404(Level, id=level_id)
+    questions = level.questions.all()
+    context = {
+        "level": level,
+        "questions": questions,
+    }
+
+    return render(request, "quizzes/quiz.html", context)
