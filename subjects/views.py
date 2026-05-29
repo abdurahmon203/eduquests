b67562a2a21aaf2cdfe68dfcc06a0b6642249f3d
Abdurahmon <nozimovislom101@gmail.com>
2026-05-29 06:01:11 +0500

    Added subjects.view for cheking

--- subjects/views.py
+++ subjects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Subject
 
-# Create your views here.
+
+def subject_list(request):
+    subjects = Subject.objects.all()
+
+    context = {"subjects": subjects}
+
+    return render(request, "subjects/subject_list.html", context)
