42d9cf9eac9f0530181e7d3be6e5a0391b4b3529
Abdurahmon <nozimovislom101@gmail.com>
2026-05-29 06:08:56 +0500

    Added level_list in views.py

--- levels/views.py
+++ levels/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from subjects.models import Subject
 
-# Create your views here.
+
+def level_list(request, subject_id):
+    subject = get_object_or_404(Subject, id=subject_id)
+    levels = subject.levels.all()
+    context = {
+        "subject": subject,
+        "levels": levels,
+    }
+
+    return render(request, "levels/level_list.html", context)
