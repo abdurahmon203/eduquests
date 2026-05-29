29ef6f232a28704471a61efc316690dca473fb89
Abdurahmon <nozimovislom101@gmail.com>
2026-05-29 05:28:09 +0500

    Quizzes Models finishing

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from levels.models import Level
 
-# Create your models here.
+
+class Question(models.Model):
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, related_name="questions")
+    question = models.TextField()
+    option_a = models.CharField(max_length=255)
+    option_b = models.CharField(max_length=255)
+    option_c = models.CharField(max_length=255)
+    option_d = models.CharField(max_length=255)
+    ANSWER_CHOICES = [
+        ("A", "A"),
+        ("B", "B"),
+        ("C", "C"),
+        ("D", "D"),
+    ]
+    correct_answer = models.CharField(max_length=1, choices=ANSWER_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Question"
+        verbose_name_plural = "Questions"
+
+    def __str__(self):
+        return self.question[:50]
