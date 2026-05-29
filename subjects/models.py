5279e094b6cc7d04634b72a90afe8225ebb32f86
Abdurahmon <nozimovislom101@gmail.com>
2026-05-29 05:25:06 +0500

    Subjects Models finishing

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    image = models.ImageField(upload_to="subjects/")
+    description = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Subject"
+        verbose_name_plural = "Subjects"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
